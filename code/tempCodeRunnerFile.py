275dfba143ac41d229b755588f54fc65944b78ce
Samy - Nyx <119658450+Nyx-Off@users.noreply.github.com>
2024-01-15 23:27:02 +0100

    Delete code/tempCodeRunnerFile.py

--- code/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-      if transitioning:
-                if self.stop is None:
-                    self.stop = self.start_x + TRANSITION_DISTANCE if self.action == retour else self.start_x - TRANSITION_DISTANCE
-                if (self.action == retour and self.x < self.stop) or (self.action != retour and self.x > self.stop):
-                    self.x += TRANSITION_SPEED if self.action == retour else -TRANSITION_SPEED
-                else:
-                    self.x = self.stop
